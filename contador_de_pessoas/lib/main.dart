f45f25e2a9420a856570562fae0355874a0ff477
Guilherme <guisil_ps@hotmail.com>
2019-08-06 16:45:00 -0300

    Adcionando projetos de Flutter

---
+++ contador_de_pessoas/lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: "Contador de Pessoas",
+      home: Stack(
+        children: <Widget>[
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Pessoas: 0",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("+1",
+                          style:
+                              TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: FlatButton(
+                      child: Text("+1",
+                          style:
+                              TextStyle(fontSize: 40.0, color: Colors.white)),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+              Text(
+                "Pode Entrar!",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0),
+              )
+            ],
+          )
+        ],
+      )));
+}
